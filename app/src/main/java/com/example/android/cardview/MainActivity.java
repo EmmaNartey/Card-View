9f7497c2123721dfa125e6a8f8db7500708a6095
joseph paddy <paddykily@gmail.com>
2019-01-09 11:40:47 +0000

    fix activity_main.xml

--- app/src/main/java/com/example/android/cardview/MainActivity.java
+++ app/src/main/java/com/example/android/cardview/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.cardview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
